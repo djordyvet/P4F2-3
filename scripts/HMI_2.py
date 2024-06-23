5604d9e93d67273e7d59a84607503ce62e838106
Pieter Roozendaal <pgc.roozendaal@student.avans.nl>
2024-06-23 20:48:03 +0200

    Update HMI_2.py

--- scripts/HMI_2.py
+++ scripts/HMI_2.py
@@ -145,7 +145,7 @@ class HMIApp:
             self.start_button.config(state="normal")
             self.stop_button.config(state="disabled")
             self.emergency_button.config(state="disabled")
-            self.nood_button.config(state="normal")
+            self.reset_button.config(state="disabled")
             # Publish stop message as a ROS message
             nood_msg = Bool()
             nood_msg.data = False 
