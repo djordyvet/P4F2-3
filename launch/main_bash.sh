8742c7161fb52c60b715189fa05e085a7d5fcb87
Pieter Roozendaal <pgc.roozendaal@student.avans.nl>
2024-06-25 14:34:25 +0200

    Update main_bash.sh

--- launch/main_bash.sh
+++ launch/main_bash.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+#roscore
+cd niryo_robot_ws
+source devel/setup.bash
+roscore
+
+#Vision
+x-terminal-emulator -e bash -c "cd niryo_robot_ws; source devel/setup.bash;roslaunch P4F2-3 nn_detector.launch;"
+x-terminal-emulator -e bash -c "roslaunch my_niryo_robot_bringup calibrate.launch; ;"
+
+#Manipulator
+x-terminal-emulator -e bash -c "cd niryo_robot_ws; source devel/setup.bash;roslaunch my_niryo_robot_bringup calibrate.launch; ;"
+
+#HMI
+x-terminal-emulator -e bash -c "cd niryo_robot_ws; source devel/setup.bash;cd niryo_robot_ws/src/P4F2-3/HMI_software; python HMI_2.py;"
+
+
+#nodes
+x-terminal-emulator -e bash -c "cd niryo_robot_ws; source devel/setup.bash;rostopic echo /robot_current_pose;"
+
 # Open a new terminal using x-terminal-emulator and execute commands
 x-terminal-emulator -e bash -c "echo 'Hello from the new terminal'; ls;"
-
-echo "Hello, World!"
-
-# List files in the current directory
-ls
-
-# Print the current date and time
-date
